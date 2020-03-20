d7955886e95af8bf5c099a3be505f2214fe75366
Connor Turland <connorturland@gmail.com>
2020-03-20 13:15:52 -0400

    fork

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,17 @@ const CONDUCTOR_CONFIG = '/_dna_connections.json'
 const DEFAULT_TIMEOUT = 5000
 
 type Call = (...segments: Array<string>) => (params: any) => Promise<any>
-type CallZome = (instanceId: string, zome: string, func: string) => (params: any) => Promise<any>
+type CallZome = (
+  instanceId: string,
+  zome: string,
+  func: string
+) => (params: any) => Promise<any>
 type OnSignal = (callback: (params: any) => void) => void
 type Close = () => Promise<any>
 
 type ConnectOpts = {
-  url?: string,
-  timeout?: number,
+  url?: string
+  timeout?: number
   wsClient?: any
 }
 
@@ -25,49 +29,66 @@ type ConnectOpts = {
  *       socket to be ready before timing out and rejecting the promise. Defaults to 5 seconds, but if you set it
  *       to 0 or null, it will never timeout.
  */
-export const connect = (opts: ConnectOpts = {}) => new Promise<{call: Call, callZome: CallZome, close: Close, onSignal: OnSignal, ws: any}>(async (fulfill, reject) => {
-  const url = opts.url || await getUrlFromConductor().catch(() => reject(
-    'Could not auto-detect DNA interface from conductor. \
-Ensure the web UI is hosted by a Holochain Conductor or manually specify url as parameter to connect'))
-  const timeout = opts.timeout || DEFAULT_TIMEOUT
-  const ws = new Client(url, opts.wsClient)
+export const connect = (opts: ConnectOpts = {}) =>
+  new Promise<{
+    call: Call
+    callZome: CallZome
+    close: Close
+    onSignal: OnSignal
+    ws: any
+  }>(async (fulfill, reject) => {
+    const url =
+      opts.url ||
+      (await getUrlFromConductor().catch(() =>
+        reject(
+          'Could not auto-detect DNA interface from conductor. \
+Ensure the web UI is hosted by a Holochain Conductor or manually specify url as parameter to connect'
+        )
+      ))
+    const timeout = opts.timeout || DEFAULT_TIMEOUT
+    const ws = new Client(url, opts.wsClient)
 
-  ws.on('open', () => console.log('WS open'))
-  ws.on('close', () => console.log('WS closed'))
-  ws.on('error', (e) => reject(e || 'Could not establish websocket connection with requested url'))
+    ws.on('open', () => console.log('WS open'))
+    ws.on('close', () => console.log('WS closed'))
+    ws.on('error', e =>
+      reject(e || 'Could not establish websocket connection with requested url')
+    )
 
-  ws.once('open', () => {
-    const call = (...methodSegments) => (params) => {
-      const method = methodSegments.length === 1 ? methodSegments[0] : methodSegments.join('/')
-      return callWhenConnected(ws, method, params, timeout)
-    }
-    const callZome = (instanceId, zome, func) => (args) => {
-      const callObject = {
-        'instance_id': instanceId,
-        zome,
-        'function': func,
-        args
+    ws.once('open', () => {
+      const call = (...methodSegments) => params => {
+        const method =
+          methodSegments.length === 1
+            ? methodSegments[0]
+            : methodSegments.join('/')
+        return callWhenConnected(ws, method, params, timeout)
       }
-      return callWhenConnected(ws, 'call', callObject, timeout)
-    }
-    const onSignal: OnSignal = (callback: (params: any) => void) => {
-      // go down to the underlying websocket connection (.socket)
-      // for a simpler API
-      ws.socket.on('message', (message: any) => {
-        if (!message) return
-        const msg = JSON.parse(message)
-        if (msg.signal || msg.instance_stats) {
-          callback(msg)
+      const callZome = (instanceId, zome, func) => args => {
+        const callObject = {
+          instance_id: instanceId,
+          zome,
+          function: func,
+          args
         }
-      })
-    }
-    // define a function which will close the websocket connection
-    const close = () => ws.close()
-    fulfill({ call, callZome, close, onSignal, ws })
+        return callWhenConnected(ws, 'call', callObject, timeout)
+      }
+      const onSignal: OnSignal = (callback: (params: any) => void) => {
+        // go down to the underlying websocket connection (.socket)
+        // for a simpler API
+        ws.socket.on('message', (message: any) => {
+          if (!message) return
+          const msg = JSON.parse(message)
+          if (msg.signal || msg.instance_stats) {
+            callback(msg)
+          }
+        })
+      }
+      // define a function which will close the websocket connection
+      const close = () => ws.close()
+      fulfill({ call, callZome, close, onSignal, ws })
+    })
   })
-})
 
-function getUrlFromConductor (): Promise<string> {
+function getUrlFromConductor(): Promise<string> {
   return fetch(CONDUCTOR_CONFIG)
     .then(data => data.json())
     .then(json => json.dna_interface.driver.port)
@@ -78,22 +99,48 @@ function getUrlFromConductor (): Promise<string> {
  * Ensure that a ws client never attempts to call when the socket is not ready
  * Instead, return a promise that resolves only when the socket is connected and the call is made
  */
-async function callWhenConnected (ws, method, payload, timeout = null) {
-  if (ws.ready) {
-    return Promise.resolve(ws.call(method, payload))
-  } else {
-    return new Promise((resolve, reject) => {
-      const timer = timeout
+async function callWhenConnected(ws, method, payload, timeout = null) {
+  return new Promise((resolve, reject) => {
+    let callTimer
+    const genCallTimer = () => {
+      return timeout
         ? setTimeout(() => {
-          reject(`Timeout while waiting for ws to connect. method: ${method}, payload: ${JSON.stringify(payload)}`)
-        }, timeout)
+            reject(
+              `Timeout occurred during ws call. method: ${method}, payload: ${JSON.stringify(
+                payload
+              )}`
+            )
+          }, timeout)
+        : null
+    }
+    const handleResult = result => {
+      clearTimeout(callTimer)
+      resolve(result)
+    }
+    if (ws.ready) {
+      callTimer = genCallTimer()
+      ws.call(method, payload)
+        .then(handleResult)
+        .catch(reject)
+    } else {
+      const connectTimer = timeout
+        ? setTimeout(() => {
+            reject(
+              `Timeout while waiting for ws to connect. method: ${method}, payload: ${JSON.stringify(
+                payload
+              )}`
+            )
+          }, timeout)
         : null
       ws.once('open', () => {
-        clearTimeout(timer)
-        ws.call(method, payload).then(resolve).catch(reject)
+        clearTimeout(connectTimer)
+        callTimer = genCallTimer()
+        ws.call(method, payload)
+          .then(handleResult)
+          .catch(reject)
       })
-    })
-  }
+    }
+  })
 }
 
 const holochainclient = { connect }
